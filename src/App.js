866889943186d1a518bf9947b9edde7e1023b759
doLikeSlug <harry.park@wisetrackers.co.kr>
2020-06-23 15:29:14 +0900

    reducer 생성

--- src/App.js
+++ src/App.js
@@ -2,25 +2,52 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+import {createStore} from "redux";
+import todoApp from "./redux/reducer/reducer";
+
+import {
+    addTodo,
+    toggleTodo,
+    setVisibilityFilter,
+    VisibilityFilters
+} from "./redux/actions/actions";
+
+
+const store = createStore(todoApp);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+    store.dispatch(addTodo('Learn about actions'))
+    store.dispatch(addTodo('Learn about reducers'))
+    store.dispatch(addTodo('Learn about store'))
+    store.dispatch(toggleTodo(0))
+    store.dispatch(toggleTodo(1))
+    store.dispatch(toggleTodo(1))
+    store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+    unsubscribe()
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
